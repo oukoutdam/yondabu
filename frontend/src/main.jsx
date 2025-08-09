972550fb9bcaa12d5ef0fd1ec6602d8eccb01bc5
Ouk Outdam <outdam.ouk103@gmail.com>
2025-08-09 14:34:24 +0900

    Add Layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import Layout from "./Layout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
-        <Route path="about" element={<div>About Page</div>} />
+        <Route element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="about" element={<div>About Page</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
