7919e120a13d565a6b7947c23ad6402df153d69b
Ouk Outdam <outdam.ouk103@gmail.com>
2025-08-10 15:15:46 +0900

    Add Allowed Origin

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ store = SheetStore()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://yondabu-frontend.onrender.com"],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
